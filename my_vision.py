701ec817f4c5fa5b8d26d347acd04f24e0a097e4
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-03-21 13:01:35 +0000

    Model training implemented on notebook

--- my_vision.py
+++ my_vision.py
@@ -2,6 +2,7 @@ import os, json
 import pandas as pd
 from torchvision.io import read_image
 from torch.utils.data import Dataset
+import cv2
 
 class classification_dataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
@@ -9,24 +10,28 @@ class classification_dataset(Dataset):
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
+        with open("image_decoder.json","r") as f:
+            self.lookup = json.load(f)
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path + ".jpg.jpg")
+
+        image = cv2.imread(img_path + ".jpg")
         label = self.img_labels.iloc[idx, 1]
+
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
         return image, label    
     
-    def human_category_label(category : int):
-        lookup = json.load("categories.json")
-        return lookup[category]
+    def human_category_label(self,category : str):
+        category = str(category)
+        return self.lookup[category]
     
-    def machine_category_label(category : str):
-        lookup = json.load("categories.json")
-        return lookup[category]
+    def machine_category_label(self,category : str):
+        category = str(category)
+        return self.lookup[category]
