7b9f581338bd0bb6d687ebe0eec8aa77f8a96ee5
Martin Klefas-Stennett <cleverbum@hotmail.com>
2023-03-28 00:13:58 +0100

    updates for live demo & removing some unused code

--- app/api.py
+++ app/api.py
@@ -12,35 +12,12 @@ from pydantic import BaseModel
 import faiss, os, shutil, uuid, time
 import numpy as np
 from datetime import datetime
+import pandas as pd
 
 from contextlib import asynccontextmanager
 
 import image_processor
 
-
-class FeatureExtractor(nn.Module):
-    def __init__(self,
-                 decoder: dict = None):
-        super(FeatureExtractor, self).__init__()
-
-        self.device = torch.device("cpu")
-        
-        self.model = torch.load('best_weights.pt',map_location=torch.device('cpu'))
-        
-        self.decoder = decoder
-
-    def forward(self, image):
-        x = self.main(image)
-        return x
-
-    def predict(self, image):
-        with torch.no_grad():
-            x = self.forward(image)
-            return x
-
-# Don't change this, it will be useful for one of the methods in the API
-class TextItem(BaseModel):
-    text: str
 
 
 start_time = datetime.now()
@@ -51,6 +28,7 @@ try:
 
 
     model = torch.load('best_weights.pt',map_location=torch.device('cpu'))
+    
     pass
 except:
     raise OSError("No Feature Extraction model found. Check that you have the decoder and the model in the correct location")
@@ -59,28 +37,15 @@ try:
     index = faiss.read_index('images_faiss.index')
     pass
 except:
-    raise OSError("No Image model found. Check that you have the encoder and the model in the correct location")
+    raise OSError("No FAISS index found. Check that you have the encoder and the model in the correct location")
 
-#@asynccontextmanager
-#async def lifespan(app: FastAPI):
-    #everything else on startup is already implemented
-   # image_processor.validate_output_dir("received_files")
-    
-   # yield
-    # Clean up the uploads folder
-   # for filename in os.listdir('received_files'):
-       # file_path = os.path.join('received_files', filename)
-       # try:
-       #     if os.path.isfile(file_path) or os.path.islink(file_path):
-       #         os.unlink(file_path)
-       #     elif os.path.isdir(file_path):
-       #         shutil.rmtree(file_path)
-       # except Exception as e:
-       #     print('Failed to delete %s. Reason: %s' % (file_path, e))
+try:
+    imageList =   pd.read_csv(filepath_or_buffer='training_data.csv')
+    pass
+except:
+    raise OSError("No image key dictionary found. Check that it's in the correct folder")
 
-    #print("uploads directory cleared. Bye!")
-
-app = FastAPI()#lifespan=lifespan)
+app = FastAPI()
 print("Starting server")
 
 @app.get('/healthcheck')
@@ -94,11 +59,15 @@ def healthcheck():
 
 @app.post('/healthcheck/file')
 def healthcheck_file(image: UploadFile = File(...)):
-  newPath = os.path.join(str(uuid.uuid4())+'_'+image.filename)#'received_files',str(uuid.uuid4())+'_'+image.filename)
-  with open(newPath, "wb") as buffer:
-      shutil.copyfileobj(image.file, buffer)
-
-  return {"message": newPath}
+  newPath = os.path.join(str(uuid.uuid4())+'_'+image.filename)
+  try:
+      with open(newPath, "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+      os.unlink(newPath)  
+      return {"message": newPath}
+  except Exception as ex:
+      return {"message" : ex}
+  
 
   
 @app.post('/predict/feature_embedding')
@@ -114,7 +83,7 @@ def predict_image(image: UploadFile = File(...)):
    
         })
   
-@app.post('/predict/similar_images')
+@app.post('/predict/similar_image_indices')
 def predict_combined(matches: str = 3,image: UploadFile = File(...)):
 
     newPath = os.path.join(str(uuid.uuid4())+'_'+image.filename)#'received_files',str(uuid.uuid4())+'_'+image.filename)
@@ -129,6 +98,31 @@ def predict_combined(matches: str = 3,image: UploadFile = File(...)):
 
     return JSONResponse(content={
     "similar_indices": indices.tolist(), # Return the index of similar images here
+        })
+
+@app.post('/predict/similar_images')
+def return_images(matches: str = 3,image: UploadFile = File(...)):
+
+    newPath = os.path.join(str(uuid.uuid4())+'_'+image.filename)
+    with open(newPath, "wb") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    searchEmbedding = image_processor.getEmbedding(newPath,model=model)
+    searchEmbedding = np.array(searchEmbedding,dtype="float32",ndmin=2)
+
+    nprobe = 5
+    distances, indices = index.search(x=searchEmbedding,k=matches)
+
+    image_ids = list()
+
+    for array in indices:
+        for idx in array:
+            image_ids.append(imageList.at[idx,"id"])
+
+    print(image_ids)
+
+    return JSONResponse(content={
+    "similar_images": image_ids, # Return the names of similar images here
         })
     
     
--- request.py
+++ request.py
@@ -1,8 +1,8 @@
 import requests
 
-url ='http://34.254.252.228:8080/predict/similar_images'
+url ='https://13.40.82.186/predict/similar_images'
 f = {'image': open('images/0a1baaa8-4556-4e07-a486-599c05cce76c.jpg', 'rb')}
 data = {"matches":'3'}
-response = requests.post(url, files=f, data= data)
+response = requests.post(url, files=f, data= data, verify=False)
 
 print(response.json())
